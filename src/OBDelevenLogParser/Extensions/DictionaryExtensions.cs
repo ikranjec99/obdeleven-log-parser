5e52d62abba66bbd8d38b2caa8cdebfeb69a5566
Ivan <ivan.kranjec95@gmail.com>
2026-05-02 04:07:54 +0200

    Add tests

--- src/OBDelevenLogParser/Extensions/DictionaryExtensions.cs
+++ src/OBDelevenLogParser/Extensions/DictionaryExtensions.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using OBDelevenLogParser.Models;
 
 namespace OBDelevenLogParser.Extensions;
@@ -11,18 +12,24 @@ public static class DictionaryExtensions
             FrequencyCounter:    f.Int("Malfunction frequency counter"),
             UnlearningCounter:   f.Int("Unlearning counter"),
             MileageKm:           f.Int("km-Mileage"),
-            EngineRpm:           f.Dbl("Engine speed"),
-            VehicleSpeedKmh:     f.Dbl("Vehicle speed"),
-            CoolantTempC:        f.Dbl("Coolant temperature"),
-            IntakeAirTempC:      f.Dbl("Intake air temperature"),
-            AmbientPressureMbar: f.Dbl("Ambient air pressure"),
-            Voltage:             f.Dbl("Voltage terminal 30"),
-            DynamicData:         f.Str("Dynamic environmental data"),
+            EngineRpm:           f.Double("Engine speed"),
+            VehicleSpeedKmh:     f.Double("Vehicle speed"),
+            CoolantTempC:        f.Double("Coolant temperature"),
+            IntakeAirTempC:      f.Double("Intake air temperature"),
+            AmbientPressureMbar: f.Double("Ambient air pressure"),
+            Voltage:             f.Double("Voltage terminal 30"),
+            DynamicData:         f.String("Dynamic environmental data"),
             Date:                f.TryGetValue("date", out var d) && DateTime.TryParse(d, out var dt) ? dt : null
         );
         
-        private double Dbl(string k) => f.TryGetValue(k, out var v) && double.TryParse(v.Split(' ')[0], out var n) ? n : 0;
-        private int Int(string k) => f.TryGetValue(k, out var v) && int.TryParse(v.Split(' ')[0], out var n) ? n : 0;
-        private string Str(string k) => f.GetValueOrDefault(k, "");
+        private double Double(string k) => 
+            f.TryGetValue(k, out var v) 
+            && double.TryParse(v.Split(' ')[0], CultureInfo.InvariantCulture, out var n) ? n : 0;
+        
+        private int Int(string k) => 
+            f.TryGetValue(k, out var v) 
+            && int.TryParse(v.Split(' ')[0], CultureInfo.InvariantCulture ,out var n) ? n : 0;
+        
+        private string String(string k) => f.GetValueOrDefault(k, "");
     }
 }
